b0c5d9a0741da26f75b55e17a66e5bbb3b5233be
shalvi93 <shalvi93@gmail.com>
2017-01-28 08:54:58 +0530

    Add files via upload

--- main.swift
+++ main.swift
@@ -1,52 +1,36 @@
 //
 //  main.swift
-//  demo1
+//  SHAPE
 //
 //  Created by Sierra 4 on 27/01/17.
 //  Copyright © 2017 Sierra 4. All rights reserved.
 //
 
 import Foundation
-var series : [Int] = [2,5,7,9,11,13]
-var totalNumber=series.count
-
-func mean(numbers : [Int]) -> Float
+let  pi:Float = 3.14
+let radius:Float=5
+enum Shapes
 {
-    var sum = 0
-    for number in numbers
-    {
-        sum += number
-    }
-    let mean:Float = Float(sum) / Float(numbers.count)
-    return Float(mean)
-    }
-print("mean :\(mean(numbers: series)) \n")
-
-func median(series:[Int]) -> Int
-    {
-        let sortedSeries = series.sorted()
-        
-        let midIndex = totalNumber/2
-        
-        let valueMedian:Int
-        
-        if totalNumber % 2 == 0
-        {
-            var median = (sortedSeries[midIndex]+sortedSeries[midIndex-1])/2
-            valueMedian=median
-        }
-            
-        else
-        {
-            valueMedian = sortedSeries[midIndex]
-            
-        }
-        
-        
-        
-        
-        return valueMedian
+    case area(Float)
+    case perimeter(Float)
+    case volume(Float)
+    case surfaceArea(Float)
+    
 }
 
-print("median is: \(median(series: series)) \n")
 
+class circle
+{
+    func circleArea(radius:Float) -> Float
+    {
+      let circleAre = pi*radius*radius
+        return (circleAre)
+    }
+
+func circlePerimeter(radius:Float) -> Float
+{
+   let circlePerimeter = 2*pi*radius
+    return(circlePerimeter)
+}
+}
+print(" area of circle : ) /n circlePerimeter \(circlePerimeter(radius))")
